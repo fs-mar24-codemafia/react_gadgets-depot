50c7a9196f7404a04c2b48642d14a244077561a5
denys <denys.sheremeta.web@gmail.com>
2024-06-04 21:45:52 +0300

    clean testing component

--- src/components/Footer/Footer.tsx
+++
@@ -1,5 +0,0 @@
-import './Footer.scss';
-
-export const Footer = () => {
-  return <footer>FOOTER SECTION</footer>;
-};
--- src/components/Header/Header.tsx
+++
@@ -1,3 +0,0 @@
-export const Header = () => {
-  return <div>HEADER SECTION</div>;
-};
