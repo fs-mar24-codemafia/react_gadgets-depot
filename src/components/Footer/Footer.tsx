1b30d4b67254ad533e6ebea3bb325d909f2ab807
Tetiana Veremchuk <tetianaverem@gmail.com>
2024-06-05 15:17:26 +0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,29 @@
 import './Footer.scss';
 
 export const Footer = () => {
-  return <footer>FOOTER COMPONENT</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <a href="#" className="footer__logo link">
+        </a>
+
+        <nav className="footer__nav nav">
+          <a href="#" className="footer__nav-link">
+            Github
+          </a>
+          <a href="#" className="footer__nav-link">
+            Contacts
+          </a>
+          <a href="#" className="footer__nav-link">
+            Rights
+          </a>
+        </nav>
+
+        <div className="footer__back-to-top">
+          Back to top
+          <a href="#header" className="ico-up"/>
+        </div>
+      </div>
+    </footer>
+  );
 };
