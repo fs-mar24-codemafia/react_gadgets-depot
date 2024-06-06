dd81023fc54e716dc07643aa1c71f4539eb43841
Anton Yupko <antonyupkodm@gmail.com>
2024-06-06 16:59:29 +0300

    fix/ProductCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.scss';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
-import { ProductCard } from './components/ProductCard';
 
 export const App = () => (
   <div className="App">
     <h1>Product Catalog</h1>
     <Header />
     <Footer />
-    <ProductCard />
   </div>
 );
