363a272a6eb9d40037dd78a353cd9e9d976aa49a
denys <denys.sheremeta.web@gmail.com>
2024-06-13 02:46:28 +0300

    nigth solution fix

--- src/pages/FavPage/FavPage.tsx
+++ src/pages/FavPage/FavPage.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import './FavPage.scss';
+
 import { ProductCard } from '../../components/ProductCard';
 import { BreadCrumbs } from '../../components/BreadCrumbs';
 import { FavoritesContext } from '../../contexts/FavoritesContext';
@@ -12,21 +13,21 @@ export const FavPage = () => {
       {!favorites.length ? (
         'Empty'
       ) : (
-        <div className="products">
-          <div className="products__top">
+        <div className="favourites">
+          <div className="favourites__top">
             <BreadCrumbs />
 
             <div>
-              <h1 className="products__title">Favourite products</h1>
-              <p className="products__amount">
+              <h1 className="favourites__title">Favourite products</h1>
+              <p className="favourites__amount">
                 {favorites.length} product{favorites.length === 1 ? '' : 's'}{' '}
                 added to favourites
               </p>
             </div>
           </div>
-          <ul className="products__list">
+          <ul className="favourites__list">
             {favorites.map(product => (
-              <li key={product.id} className="products__item">
+              <li key={product.id} className="favourites__item">
                 <ProductCard product={product} />
               </li>
             ))}
