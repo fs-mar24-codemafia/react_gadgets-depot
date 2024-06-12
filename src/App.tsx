faafd66bd41387b37cc1f52926c956b51a674486
denys <denys.sheremeta.web@gmail.com>
2024-06-12 21:53:41 +0300

    add cat image

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.scss';
 
 import { Header } from './components/Header';
 import { NewFooter } from './components/NewFooter';
-// import { NotFoundPage } from './pages/NotFoundPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 import { HomePage } from './pages/HomePage';
 import { ProductsPage } from './pages/ProductsPage/ProductsPage';
 import { CartPage } from './pages/CartPage';
@@ -15,26 +15,25 @@ export const App = () => (
     <Header />
 
     <main>
-    <Routes>
+      <Routes>
         <Route path="/">
           <Route index element={<HomePage />} />
           <Route path="phones">
-            <Route index element={<ProductsPage category='phones' />} />
+            <Route index element={<ProductsPage category="phones" />} />
             <Route path=":itemId" element={<ItemPage />} />
           </Route>
           <Route path="tablets">
-            <Route index element={<ProductsPage category='tablets' />} />
+            <Route index element={<ProductsPage category="tablets" />} />
             <Route path=":itemId" element={<ItemPage />} />
           </Route>
           <Route path="accessories">
-            <Route index element={<ProductsPage category='tablets' />} />
+            <Route index element={<ProductsPage category="tablets" />} />
             <Route path=":itemId" element={<ItemPage />} />
           </Route>
           <Route path="favourites" element={<p>Favourites</p>} />
           <Route path="cart" element={<CartPage />} />
           <Route path="home" element={<Navigate to="/" replace />} />
-          {/* <Route path="*" element={<NotFoundPage />} /> */}
-          <Route path="*" element={<p>page not found</p>} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </main>
