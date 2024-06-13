e458938552f36d8eb1663db786a3844289812cff
Mariia Lutska <mariialutskaya@gmail.com>
2024-06-13 16:04:51 +0300

    bug fix and improvement of pagination

--- src/pages/ProductsPage/Pagination/Pagination.tsx
+++ src/pages/ProductsPage/Pagination/Pagination.tsx
@@ -9,14 +9,82 @@ interface Props {
   onPageChange: (value: number) => void;
 }
 
-const getNumbers = (from: number, to: number): number[] => {
-  const numbers = [];
+const getPaginationNumbers = (currentPage: number, lastPage: number) => {
+  const maxLength = 7;
+  const res: Array<number> = [];
 
-  for (let n = from; n <= to; n += 1) {
-    numbers.push(n);
+  if (lastPage <= maxLength) {
+    for (let i = 1; i <= lastPage; i++) {
+      res.push(i);
+    }
+  } else {
+    const firstPage = 1;
+    const confirmedPagesCount = 3;
+    const deductedMaxLength = maxLength - confirmedPagesCount;
+    const sideLength = deductedMaxLength / 2;
+
+    if (
+      currentPage - firstPage < sideLength ||
+      lastPage - currentPage < sideLength
+    ) {
+      for (let j = 1; j <= sideLength + firstPage; j++) {
+        res.push(j);
+      }
+
+      res.push(NaN);
+
+      for (let k = lastPage - sideLength; k <= lastPage; k++) {
+        res.push(k);
+      }
+    } else if (
+      currentPage - firstPage >= deductedMaxLength &&
+      lastPage - currentPage >= deductedMaxLength
+    ) {
+      const deductedSideLength = sideLength - 1;
+
+      res.push(1);
+      res.push(NaN);
+
+      for (
+        let l = currentPage - deductedSideLength;
+        l <= currentPage + deductedSideLength;
+        l++
+      ) {
+        res.push(l);
+      }
+
+      res.push(NaN);
+      res.push(lastPage);
+
+    } else {
+      const isNearFirstPage = currentPage - firstPage < lastPage - currentPage;
+      let remainingLength = maxLength;
+
+      if (isNearFirstPage) {
+        for (let m = 1; m <= currentPage + 1; m++) {
+          res.push(m);
+          remainingLength -= 1;
+        }
+
+        res.push(NaN);
+        remainingLength -= 1;
+      } else {
+        for (let o = lastPage; o >= currentPage - 1; o--) {
+          res.unshift(o);
+          remainingLength -= 1;
+        }
+
+        res.unshift(NaN);
+        remainingLength -= 1;
+
+        for (let p = remainingLength; p >= 1; p--) {
+          res.unshift(p);
+        }
+      }
+    }
   }
 
-  return numbers;
+  return res;
 };
 
 export const Pagination: FC<Props> = ({
@@ -24,7 +92,7 @@ export const Pagination: FC<Props> = ({
   currentPage,
   onPageChange,
 }) => {
-  const pagesNumbers = getNumbers(1, totalPages);
+  const pagesNumbers = getPaginationNumbers(currentPage, totalPages);
   const prevPageButtonHidden = currentPage === 1;
   const nextPageButtonHidden =
     currentPage === pagesNumbers[pagesNumbers.length - 1];
@@ -50,19 +118,29 @@ export const Pagination: FC<Props> = ({
         </button>
       </li>
 
-      {pagesNumbers.map(pageNumber => (
-        <li key={pageNumber} className="pagination__item">
-          {' '}
-          <button
-            className={cn('pagination__page', {
-              'pagination__page--active': isPageActive(pageNumber),
-            })}
-            onClick={() => onPageChange(pageNumber)}
-          >
-            {pageNumber}
-          </button>
-        </li>
-      ))}
+      {pagesNumbers.map((pageNumber, id) => {
+        if (isNaN(pageNumber)) {
+          return (
+            <li key={'plug' + id} className='pagination__item pagination__item--plug'>
+              <p className='pagination__plug'>...</p>
+            </li>
+          )
+        }
+
+        return (
+          <li key={pageNumber} className="pagination__item">
+            {' '}
+            <button
+              className={cn('pagination__page', {
+                'pagination__page--active': isPageActive(pageNumber),
+              })}
+              onClick={() => onPageChange(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          </li>
+        )
+      })}
 
       <li
         className={cn('pagination__item pagination__item--right', {
