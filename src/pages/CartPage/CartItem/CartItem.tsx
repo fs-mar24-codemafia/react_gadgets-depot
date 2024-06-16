6b2edb009be3d018ca428c48083f7f603aff9d6f
denys <denys.sheremeta.web@gmail.com>
2024-06-16 14:47:40 +0300

    cart page fix

--- src/pages/CartPage/CartItem/CartItem.tsx
+++ src/pages/CartPage/CartItem/CartItem.tsx
@@ -29,57 +29,11 @@ export const CartItem: React.FC<Props> = ({
   return (
     <div className="cart-item">
       <div className="gadget">
-      <button
-        type="button"
-        aria-label="delete button"
-        className="cart-item__button-delete ico ico-close"
-        name="Delete from cart"
-        title="Delete from cart"
-        onClick={() => deleteItemFromCart(product.id)}
-      />
-      <Link
-        className="cart-item__product-link"
-        to={`/${product.category}/${product.itemId}`}
-      >
-        <img
-          src={product.image}
-          alt={product.name}
-          className="cart-item__photo"
-        />
-      </Link>
-
-      <Link
-        to={`/${product.category}/${product.itemId}`}
-        className="cart-item__product-link cart-item__product-link--name"
-      >
-        <h2 className="cart-item__name">{product.name}</h2>
-      </Link>
-      </div>
-
-
-
-      <div className="sum">
-
-      <div className="cart-item__count-buttons">
         <button
           type="button"
-          disabled={quantity === 1}
-          className={cn('cart-item__button ico ico-minus', {
-            'ico-disabled-minus': quantity === 1,
-          })}
-          aria-label="minus"
-          name="Decrease amount"
-          title="Decrease amount"
-          onClick={() => decreaseAmount(product.id)}
-        />
-        {quantity}
-        <button
-          type="button"
-          className="cart-item__button ico ico-plus"
-          aria-label="plus"
-          name="Increase amount"
-          title="Increase amount"
-          onClick={() => increaseAmount(product.id)}
+          aria-label="delete button"
+          className="cart-item__button-delete ico ico-close"
+          onClick={() => deleteItemFromCart(product.id)}
         />
         <Link
           className="cart-item__product-link"
